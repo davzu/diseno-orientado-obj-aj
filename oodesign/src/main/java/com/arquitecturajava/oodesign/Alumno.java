3973674678a710b274f318ebed9f617bc952ab5a
davzu <davzu_90@outlook.com>
2022-05-27 23:59:19 -0500

    Modulo 1 - Introduccion

---
+++ oodesign/src/main/java/com/arquitecturajava/oodesign/Alumno.java
@@ -0,0 +1,82 @@
+package com.arquitecturajava.oodesign;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Objects;
+
+public class Alumno {
+	
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private LocalDate fechaNacimiento;
+	
+	public Alumno() {
+		super();
+	}
+
+	public Alumno(String dni) {
+		super();
+		this.dni = dni;
+	}
+	
+	public Alumno(String dni, String nombre, String apellidos, LocalDate fechaNacimiento) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public int getEdad() {
+		Period periodo = Period.between(fechaNacimiento, LocalDate.now());
+		return periodo.getYears();
+	}
+	
+	public boolean esMayorEdad() {
+		return getEdad() >= 18;
+	}
+	
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dni);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alumno other = (Alumno) obj;
+		return Objects.equals(dni, other.dni);
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", fechaNacimiento="
+				+ fechaNacimiento + "]";
+	}
+
+}
